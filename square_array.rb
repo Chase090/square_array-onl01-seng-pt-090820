c19cb44627ef41810bb4a802c242263d66bafa5b
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-14 23:10:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
-  new_array = []
-  array.each {|integer|  array << integer ** 2} 
-  return array
+new_array = []
+ 
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
 
